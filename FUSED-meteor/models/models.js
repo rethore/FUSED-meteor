5414efabf0397f6944720a2c3f6d251c56cee96e
Pierre-Elouan Réthoré <rethore@gmail.com>
2015-11-20 10:19:25 +0100

    making the upload directory relative

--- FUSED-meteor/models/models.js
+++ FUSED-meteor/models/models.js
@@ -2,8 +2,8 @@ Items = new Mongo.Collection("items");
 Params = new Mongo.Collection("params");
 
 Problems = new FS.Collection("problems", {
-  stores: [new FS.Store.FileSystem("problems", {path: "/Users/pire/uploads/problems"})]
+  stores: [new FS.Store.FileSystem("problems", {path: "uploads/problems"})]
 });
 Inputs = new FS.Collection("inputs", {
-  stores: [new FS.Store.FileSystem("inputs", {path: "/Users/pire/uploads/inputs"})]
+  stores: [new FS.Store.FileSystem("inputs", {path: "uploads/inputs"})]
 });
